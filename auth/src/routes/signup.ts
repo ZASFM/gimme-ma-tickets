43767b96dec6e14f9b287550ef75a25f3490345c
shafi <shafi.bahrami.2015@gmail.com>
2023-07-06 18:22:56 +0200

    [auth], feat, routes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,29 @@
-import express from 'express';
-const router=express.Router();
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
-router.post('/api/users/signup',(req,res)=>{
-   res.send('Hi')
+const router = express.Router();
+
+router.post('/api/users/signup', [
+   //validation midleware
+   body('email')
+      .isEmail()
+      .withMessage('email must be a string'),
+   body('password')
+      .trim()
+      .isLength({ max: 20, min: 4 })
+      .isString()
+      .withMessage('password should be at least between 4 and 20 characters')
+], (req: Request, res: Response) => {
+   //checking error
+   const errors=validationResult(req);
+   //returning error in case there is
+   if(!errors.isEmpty()){
+      return res.status(400).send(errors.array());
+   }
+   const { email, password } = req.body;
+   console.log('user created');
+   res.send('was app');
+   
 })
 
-export {router as signUpRouter}
+export { router as signUpRouter }
