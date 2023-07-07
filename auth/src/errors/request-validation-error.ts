09a75a05f02c0a68c82b105a4e1009b34e11c71f
shafi <shafi.bahrami.2015@gmail.com>
2023-07-07 16:56:31 +0200

    [auth], feat, errs, err handle structure

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,30 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";/* 
 
-export class RequestValidationError extends Error{
+interface CustomError{
+   statusCode:number,
+   serializeErrors():{
+      message:string,
+      field?:string
+   }[]
+} */
+
+export class RequestValidationError extends CustomError{
+   statusCode=400;
+
    constructor(public errors:ValidationError[]){
       super();
 
       Object.setPrototypeOf(this,RequestValidationError.prototype);
    }
+
+   serializeErrors() {
+      return this.errors.map((err) => {
+        if (err.type === 'field') {
+          return { message: err.msg, field: err.path };
+        }
+        return { message: err.msg };
+      });
+    }
 }
 
