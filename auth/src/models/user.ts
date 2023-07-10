dde530a929e644184bb1506104a9bf23035afa1f
shafi <shafi.bahrami.2015@gmail.com>
2023-07-10 21:28:47 +0200

    [auth], feat, hash pass

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 //interface that takes care of defining my user attributes
 interface UserAttrs{ 
@@ -36,6 +37,15 @@ const userSchema=new mongoose.Schema({
    },
    timestamps:true
 });
+
+userSchema.pre('save',async function(next){
+   if(this.isModified('password')){
+      const hashed=await Password.toHash(this.get('password'));
+      this.set('password',hashed);
+   }
+
+   next();
+})
 
 userSchema.statics.build=(attrs:UserAttrs)=>{
    return new User(attrs);
