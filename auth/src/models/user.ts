ac7aa3c7fd4af61b4c5bcf82e4fa679ae0be9d6d
shafi <shafi.bahrami.2015@gmail.com>
2023-07-10 21:10:17 +0200

    [auth], feat, save user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,22 @@
 import mongoose from "mongoose";
 
+//interface that takes care of defining my user attributes
+interface UserAttrs{ 
+   email: string,
+   password: string
+}
+
+//Interface that takes care of adding a build function to mu user model
+interface UserModel extends mongoose.Model<UserDoc>{
+   build:(attrs:UserAttrs)=>UserDoc
+}
+
+//Interface that takes care of describing the properties that a user Doc has
+interface UserDoc{
+   save(): unknown;
+   email:string,
+   password:string,
+}
 const userSchema=new mongoose.Schema({
    email:{
       type: String,
@@ -20,6 +37,13 @@ const userSchema=new mongoose.Schema({
    timestamps:true
 });
 
-const User= mongoose.model('User',userSchema);
+userSchema.statics.build=(attrs:UserAttrs)=>{
+   return new User(attrs);
+}
 
-export {User};
+
+const User= mongoose.model<UserDoc, UserModel>('User',userSchema);
+
+export {
+   User,
+};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError} from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-errors';
+import { User } from '../models/user';
 const router = express.Router();
+import { BadRequestError } from '../errors/bad-request-error';
 
 router.post('/api/users/signup', [
-   //validation midleware
+   //validation middleware
    body('email')
       .isEmail()
       .withMessage('email must be a string'),
@@ -14,18 +15,30 @@ router.post('/api/users/signup', [
       .isLength({ max: 20, min: 4 })
       .isString()
       .withMessage('password should be at least between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
    //checking error
    const errors=validationResult(req);
    //returning error in case there is
    if(!errors.isEmpty()){
       throw new RequestValidationError(errors.array());
    }
-   const { email, password } = req.body;
-   console.log('user created');
-   throw new DatabaseConnectionError();
-   res.send('was app');
-   
+
+   const {email,password}=req.body;
+   //checking if email is in user:
+   const existingUser=await User.findOne({email});
+   if(existingUser){
+      throw new BadRequestError('Email already exists');
+   }
+
+   //hash password:
+
+
+   //save user:
+   const user=User.build({email,password});
+   await user.save();
+
+   res.status(201).send(user);
+
 })
 
 export { router as signUpRouter }
