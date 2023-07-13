c6aee5969004906e0c196d78ebab29dfe4a3f647
shafi <shafi.bahrami.2015@gmail.com>
2023-07-13 13:23:31 +0200

    [auth], feat, test init

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from "express";
-import {json} from 'body-parser';
-import { currentUserRouter } from "./routes/current-user";
-import { signOutRouter } from "./routes/signout";
-import { signInRouter } from "./routes/signin";
-import { signUpRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFound } from "./errors/not-found-error";
-import mongoose, { trusted } from "mongoose";
-import cookieSession from "cookie-session";
-
-const app=express();
-app.set('trust proxy',true);
-app.use(json());
-app.use(cookieSession({
-   signed:false,
-   secure:true
-}));
-
-// current user
-app.use(currentUserRouter);
-// sign in
-app.use(signInRouter);
-// sign up
-app.use(signUpRouter);
-// sign out
-app.use(signOutRouter);
-//error handler:
-app.use(errorHandler);
-//not found error
-app.get('*',async(req,res,next)=>{
-    next(new NotFound());
-});
+import mongoose from "mongoose";
+import { app } from "./app";
 
 const start=async()=>{
    if(!process.env.JWT_SECRET) throw new Error('No JWT secret present inside pod secret')
