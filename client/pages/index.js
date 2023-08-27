005c13867f2cebc5fdb71f8b7cb32aa0e1096099
shafi <shafi.bahrami.2015@gmail.com>
2023-08-27 14:34:18 +0200

    [shafi], feat, buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import  buildClient from '../api/build-client';
 
 const page = ({ currentUser }) => {
    return (
@@ -9,24 +9,11 @@ const page = ({ currentUser }) => {
 }
 
 //I need to check if getInitialProps is getting run from the server (SSR) or the client
-//req is the request comming to the getInitalPrpos from the client, it has info about the host, the cookie and etc...
-page.getInitialProps = async ({req}) => {
-   if (typeof window === undefined) {
-      //if window does not exists (window object only availabale from the browser), then SST, and connecting from client to ingress controller> http://ingess-nginx.ingress-nginx....
-      const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx-controller.svc.cluster.local/api/user/currentuser',{
-         headers:{
-            //inside my ingress controller I have defined as host ticketing.dev, so I if I dont mention this, a 404 will be shown
-            //req.headers contains the host "ticketing.dev", and the COOKIES
-            Host:req.headers
-         }
-      });
-      return data;
-   } else {
-      //if im not in the ssr im making req from the brwoser, so use / as base url for the req, here i shall also not worry about appending headers to the req, since the headers is appended from the browser
-      const { data } = await axios.get('/api/user/currentuser');
-      return data;
-   }
-   return {};
+//context is the request comming to the getInitalPrpos from the client, it has info about the host, the cookie and etc... context.headers for ex
+page.getInitialProps = async (context) => {
+   //since inside mz helper function I have created an axios instance, I still need to attach there a .get/post/delete/update... methods
+   const {data}=await buildClient(context).get('/api/user/currentuser');
+   return data;
 }
 
 export default page;
