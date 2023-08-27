4cfbbea528f6cc3ffa54773f10a3bb8f9a69f892
shafi <shafi.bahrami.2015@gmail.com>
2023-08-27 14:14:34 +0200

    [shafi], feat, SSR + getInitalProps, client -> ingress-nginx-controller

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const page=({currentUser})=>{
+const page = ({ currentUser }) => {
    return (
       <div>
          Landing page
@@ -8,11 +8,25 @@ const page=({currentUser})=>{
    )
 }
 
-page.getInitialProps=async()=>{
-   const response=axios.get('/api/users/currentuser').catch((err) => {
-      console.log(err.message);
-    });
-    return response.data;
+//I need to check if getInitialProps is getting run from the server (SSR) or the client
+//req is the request comming to the getInitalPrpos from the client, it has info about the host, the cookie and etc...
+page.getInitialProps = async ({req}) => {
+   if (typeof window === undefined) {
+      //if window does not exists (window object only availabale from the browser), then SST, and connecting from client to ingress controller> http://ingess-nginx.ingress-nginx....
+      const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx-controller.svc.cluster.local/api/user/currentuser',{
+         headers:{
+            //inside my ingress controller I have defined as host ticketing.dev, so I if I dont mention this, a 404 will be shown
+            //req.headers contains the host "ticketing.dev", and the COOKIES
+            Host:req.headers
+         }
+      });
+      return data;
+   } else {
+      //if im not in the ssr im making req from the brwoser, so use / as base url for the req, here i shall also not worry about appending headers to the req, since the headers is appended from the browser
+      const { data } = await axios.get('/api/user/currentuser');
+      return data;
+   }
+   return {};
 }
 
 export default page;
