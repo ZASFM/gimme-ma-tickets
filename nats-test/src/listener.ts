0629eeec5ffa52aba1c54709cd598a2e0f0c084d
shafi <shafi.bahrami.2015@gmail.com>
2024-01-20 22:12:20 +0100

    config

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,32 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
-const stan = nats.connect("ticketing","abc",{
+const stan = nats.connect("ticketing",randomBytes(4).toString(),{
    url:"http://localhost:4222"
 });
 
 stan.on("connect",()=>{
    console.log("Listener connected to Nats"); 
-   const subscription=stan.subscribe("tickets:created");
-   subscription.on("message",(msg)=>{  
-      console.log(msg);
-      
+
+   //config:
+   const options = stan.subscriptionOptions(). 
+     //setting manual acknowledgment of the events
+     setManualAckMode(true);
+
+   const subscription=stan.subscribe(
+      //name of the channel im susbcribing to:
+      "tickets:created",
+      //name of the queue im subscribing to:
+      "orderServiceQueueGroup",
+      //options:
+      options
+   );
+   subscription.on("message",(msg:Message)=>{  
+      const data = msg.getData();
+      if(typeof data==="string"){
+         console.log(`Message with sequence: ${msg.getSequence()}, has message: ${data}`);
+         msg.ack();
+      }      
    })
 })
 
