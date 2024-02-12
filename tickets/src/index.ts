31c97feb0fcafa1b80addfa5f048442259e6799f
shafi <shafi.bahrami.2015@gmail.com>
2024-02-12 21:58:20 +0100

    update ticket

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
-import { natsWrapper } from "../nats-wrapper";}
+import { natsWrapper } from "../nats-wrapper";
 
 const start=async()=>{
    if(!process.env.JWT_SECRET) throw new Error('No JWT secret present inside pod secret');
@@ -8,9 +8,15 @@ const start=async()=>{
 
    try{
       await natsWrapper.connect('ticketing','abc','http://nats-srv:4222');
+      //gracefull shut
+      natsWrapper.client.on("close",()=>{
+         console.log('NATS connection closed');
+         process.exit();
+      })
+      natsWrapper.client.on("SIGINT",()=>natsWrapper.client.close());
+      natsWrapper.client.on("SIGTERM",()=>natsWrapper.client.close());
       await mongoose.connect(process.env.MONGO_URI);
       console.log('connected to mdb');
-      
    }catch(err){
       console.log(err);
    }
