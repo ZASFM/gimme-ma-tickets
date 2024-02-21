827cbb2c2299ab3078b21ab95bcf7a279892d6a3
shafi <shafi.bahrami.2015@gmail.com>
2024-02-21 19:48:44 +0100

    create order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,26 @@
-import express,{Request, Response} from 'express';
+import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@zasfmy/commontick";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post('/api/orders',async(req:Request, res:Response)=>{
-   res.send();
-})
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      //although not recomendable, checking the the incomming ticketIf from the body, is similar to mongodb id
+      //this middleware checks only if id is string, so an id of 123 would pass the check
+      //the ticketId should be a mongodb id type
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("ticketId must be a string"),
+  ],
+  async (req: Request, res: Response) => {
+    res.send();
+  }
+);
 
-export {router as newOrderRouter}
+export { router as newOrderRouter };
