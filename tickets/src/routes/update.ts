89c32753bd7376537063182de35c0bd784eb2fb5
shafi <shafi.bahrami.2015@gmail.com>
2024-02-21 19:05:08 +0100

    setup, orders

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -8,7 +8,7 @@ import {
 } from '@zasfmy/commontick';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
-import { natsWrapper } from '../../nats-wrapper';
+import { natsWrapper } from '../nats-wrapper';
 const router=express.Router();
 
 router.put(
