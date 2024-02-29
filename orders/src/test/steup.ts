889184618a5cd188fa77e4b8e951f3dd18f1b316
shafi <shafi.bahrami.2015@gmail.com>
2024-02-29 18:48:45 +0100

    testing new order

---
+++ orders/src/test/steup.ts
@@ -0,0 +1,61 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import request from 'supertest';
+import jwt from 'jsonwebtoken';
+
+//adding signin function to global interface
+declare global {
+   var signing: () => string[];
+}
+
+jest.mock("../nats-wrapper")
+
+let mongo: any;
+beforeAll(async () => {
+   process.env.JWT_SECRET = '123';
+   mongo = await MongoMemoryServer.create();
+   const mongoUri = mongo.getUri();
+   await mongoose.connect(mongoUri, {});
+})
+
+beforeEach(async () => {
+
+   //clear all mock, since between each test, it records th times it get called and args passed
+   jest.clearAllMocks();
+
+   const collections = await mongoose.connection.db.collections();
+   for (let collection of collections) {
+      await collection.deleteMany({});
+   }
+})
+
+afterAll(async () => {
+   if (mongo) {
+      await mongo.stop();
+   }
+   await mongoose.connection.close();
+});
+
+function signing() {
+   throw new Error("Function not implemented.");
+}
+
+//this signin isgonna serve to create a jwt and use it for signin for tests
+global.signing =  () => {
+   //create jwt payload: {name:'',email:''}
+   const payload = {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: 'email@email.com'
+   }
+   //create a jwt
+   const token = jwt.sign(payload, process.env.JWT_KEY!);
+   //build session obj: {jwt: 'MY_JWT'}
+   const session = { jwt: token }
+   //Turn that session into json
+   const tokenJSON = JSON.stringify(session);
+   //Take json and turn it into base64
+   const base64 = Buffer.from(tokenJSON).toString('base64');
+   //return string, supertest want an array of string as cookie, so im gonna send mzz cookie in an array
+   return [`session=${base64}`];
+}
